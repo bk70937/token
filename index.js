0f610ae817063d02dbe91a6ce3758a66ba501771
bipul <bk70937@gmail.com>
2021-10-26 14:24:52 +0530

    port change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 
