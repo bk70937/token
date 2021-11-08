16e266e2357e68f2c8a9662d6787347977c70a14
bipul <bk70937@gmail.com>
2021-11-08 18:15:16 +0530

    a

--- routes.js
+++ routes.js
@@ -76,10 +76,10 @@ router.get('/api/getNameSymbol',namesymbol);
 const Web3 = require('web3')
 const bscScan = 'https://data-seed-prebsc-1-s1.binance.org:8545/' 
 const MY_CONTRACT = require('./config/MYCONTRACT_ABI.json')
-const address = '0x84F97c2D5A5e2e99B026F3172Ea3D3303f3d48AC'
+const address = '0x5E2037763cC87D764cCE0fA72EA003afB35f1203'
 
 
-router.get('/api/getbalance', function (req,res) {
+router.get('/api/getbalance', function (req,res,next) {
 
     try{
 
@@ -87,16 +87,17 @@ router.get('/api/getbalance', function (req,res) {
         const myAddress = '0xC8Da7b2DA6B2a757AaFaC0ec5E54dC1C3ba42F7f'
         const contract = new web3.eth.Contract(MY_CONTRACT, address);
 
-        contract.methods.getBalance().call(function() {
-            web3.eth.getBalance(myAddress).then(balance => res.status(201).json(`Balance : ${balance}`))
+        contract.methods.getBalance().call(function(err, data) {
+            // web3.eth.getBalance(myAddress).then(balance => res.status(200).json(`Balance : ${balance}`))
+            return res.status(200).json({balance: data})
         })
         
     } catch(err) {
-        next(err);
+        next(err)
     }
 });
 
-router.get('/api/getreward', function (req,res) {
+router.get('/api/getreward', function (req,res,next) {
 
     try{
 
